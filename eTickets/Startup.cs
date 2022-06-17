6f91805919442050f011d4e4745e10b5d8afc5ef
AHMED <ahmedzeyad1995@gmail.com>
2022-06-17 08:44:39 +0200

    Seeding the database

--- eTickets/Startup.cs
+++ eTickets/Startup.cs
@@ -56,6 +56,9 @@ namespace eTickets
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
+
+            //Seed database
+            AppDbInitializer.Seed(app);
         }
     }
 }
