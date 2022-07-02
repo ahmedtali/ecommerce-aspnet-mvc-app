0c1532ef5aa8107edec0ed11b99dbb0c94f4027b
AHMED <ahmedzeyad1995@gmail.com>
2022-07-03 00:15:44 +0200

    Adding a new cinema

--- eTickets/Models/Cinema.cs
+++ eTickets/Models/Cinema.cs
@@ -11,11 +11,14 @@ namespace eTickets.Models
     {
         [Key]
         public int Id { get; set; }
+        [Required(ErrorMessage = "Logo is required")]
         [Display(Name ="Logo")]
         public string Logo { get; set; }
         [Display(Name = "Name")]
+        [Required(ErrorMessage = "Name is required")]
         public string Name { get; set; }
         [Display(Name = "Description")]
+        [Required(ErrorMessage = "Description is required")]
         public string Description { get; set; }
 
         //Relationships
