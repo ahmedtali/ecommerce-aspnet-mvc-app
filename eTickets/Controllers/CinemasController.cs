7ab0b3cdbec4dea86efd59d19a240700dd3ddbbc
AHMED <ahmedzeyad1995@gmail.com>
2022-07-03 00:33:35 +0200

    Getting Cinema Details(byId)

--- eTickets/Controllers/CinemasController.cs
+++ eTickets/Controllers/CinemasController.cs
@@ -37,5 +37,13 @@ namespace eTickets.Controllers
             await _service.AddAsync(cinema);
             return RedirectToAction(nameof(Index));
         }
+
+        //GET: Cinema/Detail/id
+        public async Task<IActionResult> Details(int id)
+        {
+            var cinemaDetails = await _service.GetByIdAsync(id);
+            if (cinemaDetails == null) return View("NotFound");
+            return View(cinemaDetails);
+        }
     }
 }
